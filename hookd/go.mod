77da6895f1b254ccc16ab326aa1249f7647f22e4
Kim Tore Jensen <kimtjen@gmail.com>
2019-01-18 14:23:50 +0100

    Add proper logging and command-line flags to configure it

--- hookd/go.mod
+++ hookd/go.mod
@@ -1,3 +1,6 @@
 module github.com/navikt/deployment/hookd
 
-require github.com/sirupsen/logrus v1.3.0
+require (
+	github.com/sirupsen/logrus v1.3.0
+	github.com/spf13/pflag v1.0.3
+)
--- hookd/main.go
+++ hookd/main.go
@@ -1,19 +1,90 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
 
+// Config contains the server (the webhook) cert and key.
+type Config struct {
+	ListenAddress string
+	LogFormat     string
+	LogLevel      string
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		ListenAddress: ":8080",
+		LogFormat: "text",
+		LogLevel:  "info",
+	}
+}
+
+var config = DefaultConfig()
+
+func (c *Config) addFlags() {
+	flag.StringVar(&c.ListenAddress, "listen-address", c.ListenAddress, "IP:PORT")
+	flag.StringVar(&c.LogFormat, "log-format", c.LogFormat, "Log format, either 'json' or 'text'.")
+	flag.StringVar(&c.LogLevel, "log-level", c.LogLevel, "Logging verbosity level.")
+}
+
+func textFormatter() log.Formatter {
+	return &log.TextFormatter{
+		DisableTimestamp: false,
+		FullTimestamp:    true,
+	}
+}
+
+func jsonFormatter() log.Formatter {
+	return &log.JSONFormatter{
+		TimestampFormat: time.RFC3339Nano,
+	}
+}
+
 func serve(w http.ResponseWriter, r *http.Request) {
-	log.Debug(r)
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	log.Info(string(data))
 	w.WriteHeader(200)
 }
 
-func main() {
+func run() error {
+	config.addFlags()
+	flag.Parse()
+
+	switch config.LogFormat {
+	case "json":
+		log.SetFormatter(jsonFormatter())
+	case "text":
+		log.SetFormatter(textFormatter())
+	default:
+		return fmt.Errorf("log format '%s' is not recognized", config.LogFormat)
+	}
+
+	logLevel, err := log.ParseLevel(config.LogLevel)
+	if err != nil {
+		return fmt.Errorf("while setting log level: %s", err)
+	}
+	log.SetLevel(logLevel)
+
 	http.HandleFunc("/", serve)
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: config.ListenAddress,
 	}
-	server.ListenAndServe()
+	return server.ListenAndServe()
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Errorf("Fatal error: %s", err)
+		os.Exit(1)
+	}
 }
