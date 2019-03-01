95b88b56815b65df5e3dec686ea0e6f3243eab0d
Kim Tore Jensen <kimtjen@gmail.com>
2019-03-01 14:48:39 +0100

    deployd: log correlation ID as normalized field

--- deployd/main.go
+++ deployd/main.go
@@ -63,6 +63,8 @@ func consumerLoop(consumer *cluster.Consumer, messages chan<- Message) {
 				continue
 			}
 
+			msg.Logger = *msg.Logger.WithField("delivery_id", msg.Request.CorrelationID)
+
 			messages <- msg
 
 		case err := <-consumer.Errors():
