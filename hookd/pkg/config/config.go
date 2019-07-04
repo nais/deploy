71a50642a5653173a575d618f03b0d2805974138
Kim Tore Jensen <kimtjen@gmail.com>
2019-07-04 15:40:09 +0200

    hookd: give options environment variable equivalents, and change defaults to local dev

--- hookd/pkg/config/config.go
+++ hookd/pkg/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/navikt/deployment/common/pkg/kafka"
 )
@@ -32,28 +33,45 @@ type Config struct {
 	MetricsPath   string
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func parseBool(str string) bool {
+	b, _ := strconv.ParseBool(str)
+	return b
+}
+
+func parseInt(str string) int {
+	i, _ := strconv.Atoi(str)
+	return i
+}
+
 func DefaultConfig() *Config {
 	return &Config{
-		EnableGithub:  true,
-		ListenAddress: ":8080",
-		LogFormat:     "text",
-		LogLevel:      "debug",
-		BaseURL:       "http://localhost:8080",
-		WebhookSecret: os.Getenv("GITHUB_WEBHOOK_SECRET"),
-		ApplicationID: 0,
-		InstallID:     0,
-		KeyFile:       "private-key.pem",
+		BaseURL:       getEnv("BASE_URL", "http://localhost:8080"),
+		ApplicationID: parseInt(getEnv("GITHUB_APP_ID", "0")),
+		ClientID:      getEnv("GITHUB_CLIENT_ID", ""),
+		ClientSecret:  getEnv("GITHUB_CLIENT_SECRET", ""),
+		EnableGithub:  parseBool(getEnv("GITHUB_ENABLED", "false")),
+		InstallID:     parseInt(getEnv("GITHUB_INSTALL_ID", "0")),
+		KeyFile:       getEnv("GITHUB_KEY_FILE", "private-key.pem"),
+		WebhookSecret: getEnv("GITHUB_WEBHOOK_SECRET", ""),
+		ListenAddress: getEnv("LISTEN_ADDRESS", ":8080"),
+		LogFormat:     getEnv("LOG_FORMAT", "text"),
+		LogLevel:      getEnv("LOG_LEVEL", "debug"),
 		Kafka:         kafka.DefaultConfig(),
 		S3: S3{
-			Endpoint:       "localhost:9000",
-			AccessKey:      os.Getenv("S3_ACCESS_KEY"),
-			SecretKey:      os.Getenv("S3_SECRET_KEY"),
-			BucketName:     "deployments.nais.io",
-			BucketLocation: "",
-			UseTLS:         true,
+			Endpoint:       getEnv("S3_ENDPOINT", "localhost:9000"),
+			AccessKey:      getEnv("S3_ACCESS_KEY", "accesskey"),
+			SecretKey:      getEnv("S3_SECRET_KEY", "secretkey"),
+			BucketName:     getEnv("S3_BUCKET_NAME", "deployments.nais.io"),
+			BucketLocation: getEnv("S3_BUCKET_LOCATION", ""),
+			UseTLS:         parseBool(getEnv("S3_SECURE", "false")),
 		},
-		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
-		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
-		MetricsPath:  "/metrics",
+		MetricsPath: getEnv("METRICS_PATH", "/metrics"),
 	}
 }
