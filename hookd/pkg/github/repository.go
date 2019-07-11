76b7a76ab3bd2b485a0cbf30a4a1b682d5668b0b
Kasper Furnes Bernhoff <kasper.furnes.bernhoff@nav.no>
2019-07-11 10:54:54 +0200

    [Performance Improvement] Improved repository listing speed
    So the issue is, navikt has over 900 repos and growing, even though my
    graphQL solution made it faster, the fetching is still really slow. That
    irritated me to the point, where I tried a lot of things to make it
    better, but I ended on this solution.
    
    Here i'm just using graphQL's ability to sort the repos, and getting
    them in a descending order by Creation date. With that fact i can get
    all the repos once, and just add to the list of repos as repos created
    at a newer date appear.
    
    Now! If a repo is deleted or there is a mismatch in some way all of the
    repos are fetched again, just like when it starts up for the first time.
    
    This makes the overal experience a lot more snappy while also reducing
    the amount of quries we do towards github.
    
    My solution is based on a struct containing all of the known
    repositories, aswell as the date of the repository created most
    reacently. The struct also has a R/W mutex so concurrent access won't mess
    it up.

--- hookd/pkg/github/repository.go
+++ hookd/pkg/github/repository.go
@@ -2,54 +2,99 @@ package github
 
 import (
 	"context"
+	"fmt"
+	"sync"
+	"time"
 
 	"github.com/prometheus/common/log"
 	"github.com/shurcooL/githubv4"
 )
 
-type repo struct {
-	Name                string `json:"name"`
-	NameWithOwner       string `json:"full_name"`
-	ViewerCanAdminister bool
+var (
+	repositories Repositories
+)
+
+type Repositories struct {
+	sync.RWMutex
+	List          []repo
+	lastCreatedAt time.Time
 }
 
-func GetRepositories(client *githubv4.Client) (allRepos []repo, err error) {
+type repo struct {
+	Name                string    `json:"name"`
+	NameWithOwner       string    `json:"full_name"`
+	ViewerCanAdminister bool      `json:"-"`
+	CreatedAt           time.Time `json:"-"`
+}
+
+func GetRepositories(client *githubv4.Client) (repos []repo, err error) {
 	var query struct {
 		Organization struct {
 			Repositories struct {
 				Nodes      []repo
-				TotalCount githubv4.Int
+				TotalCount int
 
 				PageInfo struct {
 					EndCursor   githubv4.String
 					HasNextPage bool
 				}
-			} `graphql:"repositories(first:100, after: $repositoriesCursor)"`
+			} `graphql:"repositories(first:100, after: $repositoriesCursor, orderBy: {field: $field, direction:$dir})"`
 		} `graphql:"organization(login: $organization)"`
 	}
 
 	variables := map[string]interface{}{
 		"organization":       githubv4.String("navikt"),
 		"repositoriesCursor": (*githubv4.String)(nil),
+		"field":              githubv4.RepositoryOrderFieldCreatedAt,
+		"dir":                githubv4.OrderDirectionDesc,
 	}
 
+Loop:
 	for {
 		err = client.Query(context.Background(), &query, variables)
 		if err != nil {
 			log.Error(err)
-			return
+			return repos, err
 		}
 
+		repositories.RLock()
 		for _, repo := range query.Organization.Repositories.Nodes {
-			if repo.ViewerCanAdminister {
-				allRepos = append(allRepos, repo)
+			if repo.CreatedAt.After(repositories.lastCreatedAt) {
+				repos = append(repos, repo)
+			} else {
+				repositories.RUnlock()
+				break Loop
 			}
 		}
+		repositories.RUnlock()
 
 		if !query.Organization.Repositories.PageInfo.HasNextPage {
 			break
 		}
 		variables["repositoriesCursor"] = githubv4.NewString(query.Organization.Repositories.PageInfo.EndCursor)
+	}
+
+	repositories.Lock()
+	defer repositories.Unlock()
+
+	allRepos := append(repos, repositories.List...)
+	if len(allRepos) != 0 && len(allRepos) == query.Organization.Repositories.TotalCount {
+		repositories.lastCreatedAt = allRepos[0].CreatedAt
+		repositories.List = allRepos
+	} else {
+		repositories.lastCreatedAt = time.Time{}
+		repositories.List = []repo{}
+		return repositories.List, fmt.Errorf("fetching new repositories, count mismatch")
+	}
+
+	return repositories.List, nil
+}
+
+func FilterRepositoriesByAdmin(allRepos []repo) (filteredRepos []repo) {
+	for _, repo := range allRepos {
+		if repo.ViewerCanAdminister {
+			filteredRepos = append(filteredRepos, repo)
+		}
 	}
 	return
 }
