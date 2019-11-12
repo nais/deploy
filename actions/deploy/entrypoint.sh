bf5f49208a5aa45ef5c004d785744e7c14757df0
Kim Tore Jensen <kimtjen@gmail.com>
2019-11-12 15:55:15 +0100

    Create GitHub action with example deploy

---
+++ actions/deploy/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+echo "::add-mask::$APIKEY"
+
+if [ -z "$OWNER" ]; then
+    export OWNER=`echo $GITHUB_REPOSITORY | cut -f1 -d/`
+fi
+
+if [ -z "$REPOSITORY" ]; then
+    export REPOSITORY=`echo $GITHUB_REPOSITORY | cut -f2 -d/`
+fi
+
+if [ -z "$QUIET" ]; then
+    export QUIET=false
+fi
+
+if [ -z "$DRY_RUN" ]; then
+    export DRY_RUN=false
+fi
+
+if [ -z "$PRINT_PAYLOAD" ]; then
+    export PRINT_PAYLOAD=false
+fi
+
+/app/deploy \
+	--apikey="$APIKEY" \
+	--cluster="$CLUSTER" \
+	--dry-run="$DRY_RUN" \
+	--owner="$OWNER" \
+	--print-payload="$PRINT_PAYLOAD" \
+	--quiet="$QUIET" \
+	--ref="$GITHUB_REF" \
+	--repository="$REPOSITORY" \
+	--resource="$RESOURCE" \
+	--team="$TEAM" \
+	--vars="$VARS" \
+	--wait="true" \
+	;
