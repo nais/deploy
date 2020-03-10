74b29c742a3d8a24208ea4e047a28393c9827987
Kim Tore Jensen <kimtjen@gmail.com>
2020-03-10 10:57:51 +0100

    amend database schema with versioned api keys and transactions

--- hookd/schema/0001-initial-database-schema.sql
+++ hookd/schema/0001-initial-database-schema.sql
@@ -1,19 +1,26 @@
+-- Run the entire migration as an atomic operation.
+START TRANSACTION ISOLATION LEVEL SERIALIZABLE READ WRITE;
+
 -- Table apikey holds teams' deploy API keys.
+-- A team can have many API keys, with each key having its own expiry time.
 CREATE TABLE apikey
 (
-    "id"       varchar primary key,
-    "apikey"   varchar,
-    "team"     varchar unique,
-    "created"  timestamp,
-    "modified" timestamp
+    "id"      varchar primary key,
+    "apikey"  varchar,
+    "team"    varchar,
+    "created" timestamp with time zone,
+    "expires" timestamp with time zone
 );
+
+-- Index teams for quick lookups.
+CREATE INDEX apikey_team_index ON apikey (team);
 
 -- Each row in the deployment table represents a single deployment request.
 CREATE TABLE deployment
 (
     "id"                varchar primary key,
     "team"              varchar,
-    "created"           timestamp,
+    "created"           timestamp with time zone,
     "github_id"         int unique,
     "github_repository" varchar
 );
@@ -26,5 +33,17 @@ CREATE TABLE deployment_status
     "status"        varchar,
     "message"       varchar,
     "github_id"     int,
-    "created"       timestamp
+    "created"       timestamp with time zone
 );
+
+-- Database migration
+CREATE TABLE migrations
+(
+    "version" int primary key,
+    "created" timestamp with time zone
+);
+
+-- Mark this database migration as completed.
+INSERT INTO migrations (version, created)
+VALUES (1, now());
+COMMIT;
