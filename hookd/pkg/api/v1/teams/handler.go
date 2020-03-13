5b58c0aabfe012d9ee837e6bcb4c7120f2c2bd6b
Frode Sundby <frodesun@gmail.com>
2020-03-13 14:52:30 +0100

    adding groups to map of strings in teamsHandler

--- hookd/pkg/api/v1/teams/handler.go
+++ hookd/pkg/api/v1/teams/handler.go
@@ -1,7 +1,6 @@
 package api_v1_teams
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,8 +8,11 @@ type TeamsHandler struct {
 }
 
 func (h *TeamsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Converting groups from claims to group slice
-
-	fmt.Printf("groups: %s\n", r.Context().Value("groups"))
-	w.Write([]byte("Got teams "))
+	groups := r.Context().Value("groups").([]string)
+	groupString := "Group claims are: \n"
+	for _, v := range groups {
+		groupString += v + "\n"
+	}
+	response := []byte(groupString)
+	w.Write(response)
 }
