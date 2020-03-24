ca70e4775222d27928b56bdd8279ea56c93a3713
Kim Tore Jensen <kimtjen@gmail.com>
2020-03-24 14:12:34 +0100

    write s3 migration tool

---
+++ cmd/migtool/main.go
@@ -0,0 +1,72 @@
+// migtool is a migration tool for converting team repository data from dumped S3 format
+// into a postgresql database.
+//
+// prerequisites:
+//
+// $ mkdir /tmp/migrate
+// $ cd /tmp/migrate
+// $ s3cmd get -r s3://deployments.nais.io/navikt
+//
+// run migtool:
+// $ migtool -source /tmp/migrate/navikt -url postgres://...
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/navikt/deployment/hookd/pkg/database"
+)
+
+func main() {
+	url := flag.String("url", "postgres://postgres:root@localhost/hookd", "postgresql url")
+	source := flag.String("source", ".", "source of data")
+
+	flag.Parse()
+
+	db, err := database.New(*url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := ioutil.ReadDir(*source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fn := range files {
+		if fn.IsDir() {
+			continue
+		}
+		path := filepath.Join(*source, fn.Name())
+		file, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		repository := "navikt/" + fn.Name()
+		teams := make([]string, 0)
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&teams)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = db.WriteRepositoryTeams(repository, teams)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Infof("wrote %s -> %v", repository, teams)
+	}
+}
