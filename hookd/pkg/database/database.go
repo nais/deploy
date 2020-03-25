47509f63ac3ea72c18c30dbddd2870260c10e9c5
Kim Tore Jensen <kimtjen@gmail.com>
2020-03-25 13:53:06 +0100

    integrated encryption and decryption of api key in database layer

--- hookd/pkg/database/database.go
+++ hookd/pkg/database/database.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/navikt/deployment/pkg/crypto"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -57,6 +58,45 @@ func New(dsn string, encryptionKey []byte) (Database, error) {
 	}, nil
 }
 
+func (db *database) decrypt(encrypted string) (string, error) {
+	decoded, err := hex.DecodeString(encrypted)
+	if err != nil {
+		return "", fmt.Errorf("decode hex: %s", err)
+	}
+	decrypted, err := crypto.Decrypt(decoded, db.encryptionKey)
+	if err != nil {
+		return "", fmt.Errorf("decrypt: %s", err)
+	}
+	return hex.EncodeToString(decrypted), nil
+}
+
+func (db *database) scanApiKeyRows(rows pgx.Rows) ([]ApiKey, error) {
+	apiKeys := make([]ApiKey, 0)
+
+	for rows.Next() {
+		var apiKey ApiKey
+		var encrypted string
+
+		err := rows.Scan(&encrypted, &apiKey.Team, &apiKey.GroupId, &apiKey.Created, &apiKey.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		apiKey.Key, err = db.decrypt(encrypted)
+		if err != nil {
+			return nil, err
+		}
+
+		apiKeys = append(apiKeys, apiKey)
+	}
+
+	if len(apiKeys) == 0 {
+		return nil, ErrNotFound
+	}
+
+	return apiKeys, nil
+}
+
 func (db *database) Migrate() error {
 	ctx := context.Background()
 	var version int
@@ -87,7 +127,6 @@ func (db *database) Migrate() error {
 
 func (db *database) ReadByGroupClaim(group string) ([]ApiKey, error) {
 	ctx := context.Background()
-	apiKeys := []ApiKey{}
 
 	query := `SELECT key, team, team_azure_id, created, expires FROM apikey WHERE team_azure_id = $1 ORDER BY expires DESC;`
 	rows, err := db.conn.Query(ctx, query, group)
@@ -96,20 +135,11 @@ func (db *database) ReadByGroupClaim(group string) ([]ApiKey, error) {
 		return nil, err
 	}
 
-	for rows.Next() {
-		apiKey := ApiKey{}
-		err := rows.Scan(&apiKey.Key, &apiKey.Team, &apiKey.GroupId, &apiKey.Created, &apiKey.Expires)
-		if err != nil {
-			return nil, err
-		}
-		apiKeys = append(apiKeys, apiKey)
-	}
-	return apiKeys, nil
+	return db.scanApiKeyRows(rows)
 }
 
 func (db *database) Read(team string) ([]ApiKey, error) {
 	ctx := context.Background()
-	apiKeys := []ApiKey{}
 
 	query := `SELECT key, team, team_azure_id, created, expires FROM apikey WHERE team = $1 AND expires > NOW();`
 	rows, err := db.conn.Query(ctx, query, team)
@@ -118,26 +148,15 @@ func (db *database) Read(team string) ([]ApiKey, error) {
 		return nil, err
 	}
 
-	for rows.Next() {
-		apiKey := ApiKey{}
-		err := rows.Scan(&apiKey.Key, &apiKey.Team, &apiKey.GroupId, &apiKey.Created, &apiKey.Expires)
-		if err != nil {
-			return nil, err
-		}
-		apiKeys = append(apiKeys, apiKey)
-	}
-	if len(apiKeys) == 0 {
-		return nil, ErrNotFound
-	}
-	return apiKeys, nil
+	return db.scanApiKeyRows(rows)
 }
 
 func (db *database) ReadAll(team, limit string) ([]ApiKey, error) {
-	ctx := context.Background()
-	apiKeys := make([]ApiKey, 0)
 	var query string
 	var rows pgx.Rows
 	var err error
+
+	ctx := context.Background()
 
 	if limit == "" {
 		query = `SELECT key, team, team_azure_id, created, expires FROM apikey WHERE team = $1 ORDER BY expires DESC`
@@ -151,24 +170,16 @@ func (db *database) ReadAll(team, limit string) ([]ApiKey, error) {
 		return nil, err
 	}
 
-	for rows.Next() {
-		apiKey := ApiKey{}
-		err := rows.Scan(&apiKey.Key, &apiKey.Team, &apiKey.GroupId, &apiKey.Created, &apiKey.Expires)
-		if err != nil {
-			return nil, err
-		}
-		apiKeys = append(apiKeys, apiKey)
-	}
-
-	if len(apiKeys) == 0 {
-		return nil, ErrNotFound
-	}
-
-	return apiKeys, nil
+	return db.scanApiKeyRows(rows)
 }
 
 func (db *database) Write(team, groupId string, key []byte) error {
 	var query string
+
+	encrypted, err := crypto.Encrypt(key, db.encryptionKey)
+	if err != nil {
+		return fmt.Errorf("encrypt api key: %s", err)
+	}
 
 	ctx := context.Background()
 
@@ -187,7 +198,7 @@ func (db *database) Write(team, groupId string, key []byte) error {
 INSERT INTO apikey (key, team, team_azure_id, created, expires)
 VALUES ($1, $2, $3, NOW(), NOW()+MAKE_INTERVAL(years := 5));
 `
-	_, err = tx.Exec(ctx, query, hex.EncodeToString(key), team, groupId)
+	_, err = tx.Exec(ctx, query, hex.EncodeToString(encrypted), team, groupId)
 	if err != nil {
 		return err
 	}
