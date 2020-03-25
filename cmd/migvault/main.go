39d265174fab0d2ed6935932ad35014abf987fcd
Kim Tore Jensen <kimtjen@gmail.com>
2020-03-25 13:28:41 +0100

    add vault to postgresql migration tool

---
+++ cmd/migvault/main.go
@@ -0,0 +1,202 @@
+// migvault is a migration tool for migrating API keys from Vault into a postgresql database.
+//
+// prerequisites:
+//
+// * Vault access through navtunnel proxy
+// * PostgreSQL access
+// * teams.yml file from the navikt/teams repository
+//
+// you must configure all of these:
+//
+// export AZURE_CLIENT_ID=
+// export AZURE_CLIENT_SECRET=
+// export AZURE_DISCOVERY_URL=
+// export AZURE_TEAM_MEMBERSHIP_APP_ID=
+// export AZURE_TENANT=
+// export POSTGRESQL_URL=
+// export VAULT_ADDRESS=
+// export VAULT_AUTH_PATH=
+// export VAULT_AUTH_ROLE=
+// export VAULT_CREDENTIALS_FILE=
+// export TEAMS_YAML_FILE=
+//
+// run migvault:
+//
+// go build -o migvault *.go && ./migvault
+
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/navikt/deployment/pkg/crypto"
+	flag "github.com/spf13/pflag"
+	"golang.org/x/net/proxy"
+
+	"github.com/navikt/deployment/hookd/pkg/azure/graphapi"
+	"github.com/navikt/deployment/hookd/pkg/config"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+
+	"github.com/navikt/deployment/hookd/pkg/database"
+)
+
+type Config struct {
+	CredentialsFile string
+	Token           string
+	Address         string
+	Path            string
+	AuthPath        string
+	AuthRole        string
+	KeyName         string
+	PostgresURL     string
+	TeamsYamlFile   string
+	EncryptionKey   string
+	Azure           config.Azure
+}
+
+type Team struct {
+	Name string
+}
+
+type TeamsYaml struct {
+	Teams []Team
+}
+
+const (
+	socksProxyAddress = "localhost:14122" // navtunnel
+)
+
+var cfg = Config{
+	CredentialsFile: getEnv("VAULT_CREDENTIALS_FILE", ""),
+	Address:         getEnv("VAULT_ADDRESS", "http://localhost:8200"),
+	KeyName:         getEnv("VAULT_KEY_NAME", "key"),
+	Path:            getEnv("VAULT_PATH", "/v1/apikey/nais-deploy"),
+	AuthPath:        getEnv("VAULT_AUTH_PATH", "/v1/auth/kubernetes/login"),
+	AuthRole:        getEnv("VAULT_AUTH_ROLE", ""),
+	Token:           getEnv("VAULT_TOKEN", "123456789"),
+	PostgresURL:     getEnv("POSTGRESQL_URL", "postgres://postgres:root@localhost/hookd"),
+	TeamsYamlFile:   getEnv("TEAMS_YAML_FILE", "/dev/null"),
+	EncryptionKey:   getEnv("ENCRYPTION_KEY", "ab54620542594320965429432097564295324013249013209175aabbccddeefa"),
+	Azure: config.Azure{
+		ClientID:            getEnv("AZURE_CLIENT_ID", ""),
+		ClientSecret:        getEnv("AZURE_CLIENT_SECRET", ""),
+		Tenant:              getEnv("AZURE_TENANT", ""),
+		DiscoveryURL:        getEnv("AZURE_DISCOVERY_URL", "https://login.microsoftonline.com/common/discovery/v2.0/keys"),
+		TeamMembershipAppID: getEnv("AZURE_TEAM_MEMBERSHIP_APP_ID", ""),
+	},
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func main() {
+	flag.StringVar(&cfg.Path, "vault-path", cfg.Path, "Base path to Vault KV API key store.")
+	flag.StringVar(&cfg.AuthPath, "vault-auth-path", cfg.AuthPath, "Path to Vault authentication endpoint.")
+	flag.StringVar(&cfg.AuthRole, "vault-auth-role", cfg.AuthRole, "Role used for Vault authentication.")
+	flag.StringVar(&cfg.Address, "vault-address", cfg.Address, "Address to Vault server.")
+	flag.StringVar(&cfg.KeyName, "vault-key-name", cfg.KeyName, "API keys are stored in this key.")
+	flag.StringVar(&cfg.CredentialsFile, "vault-credentials-file", cfg.CredentialsFile, "Credentials for authenticating against Vault retrieved from this file (overrides --vault-token).")
+	flag.StringVar(&cfg.Token, "vault-token", cfg.Token, "Vault static token.")
+	flag.StringVar(&cfg.PostgresURL, "postgresql-url", cfg.PostgresURL, "postgresql url")
+	flag.StringVar(&cfg.TeamsYamlFile, "teams-yaml-file", cfg.TeamsYamlFile, "path to teams.yaml from navikt/teams repository")
+	flag.StringVar(&cfg.EncryptionKey, "encryption-key", cfg.EncryptionKey, "encryption key for team keys")
+	flag.StringVar(&cfg.Azure.ClientID, "azure.clientid", cfg.Azure.ClientID, "Azure ClientId.")
+	flag.StringVar(&cfg.Azure.ClientSecret, "azure.clientsecret", cfg.Azure.ClientSecret, "Azure ClientSecret")
+	flag.StringVar(&cfg.Azure.DiscoveryURL, "azure.discoveryurl", cfg.Azure.DiscoveryURL, "Azure DiscoveryURL")
+	flag.StringVar(&cfg.Azure.Tenant, "azure.tenant", cfg.Azure.Tenant, "Azure Tenant")
+	flag.StringVar(&cfg.Azure.TeamMembershipAppID, "azure.teamMembershipAppID", cfg.Azure.TeamMembershipAppID, "Application ID of canonical team list")
+
+	flag.Parse()
+
+	dialer, err := proxy.SOCKS5("tcp", socksProxyAddress, nil, proxy.Direct)
+	if err != nil {
+		log.Fatalf("proxy: %s", err)
+	}
+
+	transport := &http.Transport{}
+	transport.Dial = dialer.Dial
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
+	apiKeys := &VaultApiKeyStorage{
+		Address:    cfg.Address,
+		Path:       cfg.Path,
+		AuthPath:   cfg.AuthPath,
+		AuthRole:   cfg.AuthRole,
+		KeyName:    cfg.KeyName,
+		Token:      cfg.Token,
+		Refreshed:  make(chan interface{}, 1300),
+		HttpClient: httpClient,
+	}
+
+	if len(cfg.CredentialsFile) > 0 {
+		credentials, err := ioutil.ReadFile(cfg.CredentialsFile)
+		if err != nil {
+			log.Fatalf("read Vault token file: %s", err)
+		}
+		apiKeys.Credentials = string(credentials)
+		apiKeys.Token = ""
+
+		go apiKeys.RefreshLoop()
+		log.Infof("waiting for vault token...")
+		<-apiKeys.Refreshed
+		log.Infof("vault token ok")
+	}
+
+	db, err := database.New(cfg.PostgresURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graphAPIClient := graphapi.NewClient(cfg.Azure)
+
+	file, err := os.Open(cfg.TeamsYamlFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	teams := &TeamsYaml{}
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(teams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encryptionKey, err := hex.DecodeString(cfg.EncryptionKey)
+	if err != nil {
+		log.Fatalf("encryption key: %s", err)
+	}
+
+	for _, team := range teams.Teams {
+		log.Infof("processing %s", team.Name)
+
+		apiKey, err := apiKeys.Read(team.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		azureTeam, err := graphAPIClient.Team(context.Background(), team.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		encryptedKey, err := crypto.Encrypt(apiKey, encryptionKey)
+		if err != nil {
+			log.Fatalf("encrypting key: %s", err)
+		}
+
+		err = db.Write(team.Name, azureTeam.AzureUUID, encryptedKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
---
+++ cmd/migvault/vault.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	ErrNotFound = fmt.Errorf("api key not found")
+)
+
+const (
+	NotFoundMessage          = "The specified key does not exist."
+	RefreshIntervalFactor    = 0.8
+	InitialTokenWaitDuration = 1 * time.Millisecond
+)
+
+type ApiKeyStorage interface {
+	Read(team string) ([]byte, error)
+	Write(team string, key []byte) error
+	IsErrNotFound(err error) bool
+}
+
+type VaultApiKeyStorage struct {
+	Address       string
+	Path          string
+	AuthPath      string
+	AuthRole      string
+	KeyName       string
+	Credentials   string
+	Token         string
+	Refreshed     chan interface{}
+	LeaseDuration int
+	HttpClient    *http.Client
+}
+
+type VaultResponse struct {
+	Data map[string]string `json:"data"`
+}
+
+type VaultAuthRequest struct {
+	JWT  string `json:"jwt"`
+	Role string `json:"role"`
+}
+
+type VaultAuthResponse struct {
+	Auth struct {
+		ClientToken   string `json:"client_token"`
+		LeaseDuration int    `json:"lease_duration"`
+	} `json:"auth"`
+}
+
+type VaultWriteRequest struct {
+	Key string `json:"key"`
+}
+
+func (s *VaultApiKeyStorage) refreshToken() error {
+	u, err := url.Parse(s.Address)
+
+	if err != nil {
+		return fmt.Errorf("unable to construct auth URL: %s", err)
+	}
+
+	u.Path = s.AuthPath
+	b, err := json.Marshal(VaultAuthRequest{JWT: s.Credentials, Role: s.AuthRole})
+
+	if err != nil {
+		return fmt.Errorf("unable to marshal auth request: %s", err)
+	}
+
+	resp, err := s.HttpClient.Post(u.String(), "application/json", bytes.NewReader(b))
+	if resp != nil {
+		log.Infof("vault returned token request with status %s", resp.Status)
+		if resp.StatusCode >= 400 {
+			body, _ := ioutil.ReadAll(resp.Body)
+			return fmt.Errorf("vault responded with %s: %s", resp.Status, body)
+		}
+	}
+
+	if err != nil {
+		return fmt.Errorf("unable to perform auth request: %s", err)
+	}
+
+	var vaultAuthResponse VaultAuthResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&vaultAuthResponse); err != nil {
+		return fmt.Errorf("unable to decode auth response: %s", err)
+	}
+
+	s.Token = vaultAuthResponse.Auth.ClientToken
+
+	s.LeaseDuration = vaultAuthResponse.Auth.LeaseDuration
+	log.Infof("Vault: token expires in %d seconds", s.LeaseDuration)
+
+	return nil
+}
+
+func (s *VaultApiKeyStorage) RefreshLoop() {
+	timer := time.NewTimer(InitialTokenWaitDuration)
+
+	for range timer.C {
+		if err := s.refreshToken(); err != nil {
+			log.Errorf("Unable to refresh Vault token: %s", err)
+			timer.Reset(1 * time.Minute)
+			continue
+		}
+		refreshInterval := int(float64(s.LeaseDuration) * RefreshIntervalFactor)
+		duration := time.Duration(refreshInterval) * time.Second
+		timer.Reset(duration)
+		log.Infof("Successfully refreshed Vault token, next refresh in %s", duration.String())
+		s.Refreshed <- new(interface{})
+	}
+}
+
+func (s *VaultApiKeyStorage) Read(team string) ([]byte, error) {
+	u, err := url.Parse(s.Address)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct URL to Vault: %s", err)
+	}
+
+	u.Path = path.Join(s.Path, team)
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to create HTTP request: %s", err)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.Token))
+
+	resp, err := s.HttpClient.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to get key from Vault: %s", err)
+	}
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		var vaultResp VaultResponse
+		decoder := json.NewDecoder(resp.Body)
+		defer resp.Body.Close()
+		if err := decoder.Decode(&vaultResp); err != nil {
+			return nil, fmt.Errorf("unable to unmarshal response from Vault: %s", err)
+		}
+
+		return hex.DecodeString(vaultResp.Data[s.KeyName])
+	case http.StatusNotFound:
+		return nil, ErrNotFound
+	default:
+		body, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("Vault returned HTTP %d: %s", resp.StatusCode, string(body))
+	}
+}
+
+func (s *VaultApiKeyStorage) Write(team string, key []byte) error {
+	u, err := url.Parse(s.Address)
+
+	if err != nil {
+		return fmt.Errorf("unable to construct URL to Vault: %s", err)
+	}
+
+	u.Path = path.Join(s.Path, team)
+
+	writeRequest := &VaultWriteRequest{
+		Key: hex.EncodeToString(key),
+	}
+	payload, err := json.Marshal(writeRequest)
+	if err != nil {
+		return fmt.Errorf("create api key payload: %s", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewReader(payload))
+
+	if err != nil {
+		return fmt.Errorf("unable to create HTTP request: %s", err)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.Token))
+	req.Header.Set("content-type", "application/json")
+
+	resp, err := s.HttpClient.Do(req)
+
+	if err != nil {
+		return fmt.Errorf("write team API key to Vault: %s", err)
+	} else if resp != nil && resp.StatusCode >= 400 {
+		errmsg, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("write team API key to Vault: %s; %s", resp.Status, errmsg)
+	}
+
+	log.Infof("Wrote Vault team API key for team '%s', response was %s", team, resp.Status)
+
+	return nil
+}
+
+func (s *VaultApiKeyStorage) IsErrNotFound(err error) bool {
+	return err == ErrNotFound
+}
+
+type StaticKeyApiKeyStorage struct {
+	Key []byte
+}
+
+func (s *StaticKeyApiKeyStorage) Read(team string) ([]byte, error) {
+	return s.Key, nil
+}
+
+func (s *StaticKeyApiKeyStorage) IsErrNotFound(err error) bool {
+	return true
+}
