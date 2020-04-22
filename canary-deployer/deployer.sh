50aa7a9156e799f6744b645bfdfc747d2aa09b30
Kim Tore Jensen <kimtjen@gmail.com>
2020-04-22 12:00:55 +0200

    truncate nanoseconds in canary deployer script

--- canary-deployer/deployer.sh
+++ canary-deployer/deployer.sh
@@ -5,7 +5,7 @@ export VARS=`mktemp`
 for CLUSTER in $CLUSTERS; do
   export CLUSTER
   echo "---" > $VARS
-  echo "now: $(date +%s%N)" >> $VARS
+  echo "now: $(date +%s)000000000" >> $VARS
   echo "fqdn: nais-deploy-canary.$CLUSTER.nais.io" >> $VARS
   echo "Deploying to $CLUSTER..."
   /app/deploy --wait=false
