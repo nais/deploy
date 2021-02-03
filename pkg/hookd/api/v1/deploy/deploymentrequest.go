096beb0e1b8915f4fcec06d230da55074f407d13
Kim Tore Jensen <kimtjen@gmail.com>
2021-02-03 11:36:05 +0100

    remove obsolete code

--- pkg/hookd/api/v1/deploy/deploymentrequest.go
+++ pkg/hookd/api/v1/deploy/deploymentrequest.go
@@ -1,13 +1,9 @@
 package api_v1_deploy
 
 import (
-	"encoding/json"
-	"fmt"
 	"time"
 
-	gh "github.com/google/go-github/v27/github"
 	types "github.com/navikt/deployment/pkg/pb"
-	"github.com/navikt/deployment/pkg/hookd/github"
 )
 
 var (
@@ -43,52 +39,5 @@ func DeploymentRequestMessage(r *DeploymentRequest, deliveryID string) (*types.D
 		Cluster:    r.Cluster,
 		Time:       types.TimeAsTimestamp(now),
 		Deadline:   now.Add(ttl).Unix(),
-	}, nil
-}
-
-// DeploymentRequestFromEvent creates a deployment request from a Github Deployment Event.
-// The event is validated, and if any fields are missing, an error is returned.
-// Any error from this function should be considered user error.
-func DeploymentRequestFromEvent(ev *gh.DeploymentEvent, deliveryID string) (*types.DeploymentRequest, error) {
-	repo := ev.GetRepo()
-	if repo == nil {
-		return nil, fmt.Errorf("no repository specified")
-	}
-
-	owner, name, err := github.SplitFullname(repo.GetFullName())
-	if err != nil {
-		return nil, err
-	}
-
-	deployment := ev.GetDeployment()
-	if deployment == nil {
-		return nil, fmt.Errorf("deployment object is empty")
-	}
-
-	cluster := deployment.GetEnvironment()
-	if len(cluster) == 0 {
-		return nil, fmt.Errorf("environment is not specified")
-	}
-
-	payload, err := types.PayloadFromJSON(deployment.Payload)
-	err = json.Unmarshal(deployment.Payload, payload)
-	if err != nil {
-		return nil, fmt.Errorf("payload is invalid: %s", err)
-	}
-
-	now := time.Now()
-	return &types.DeploymentRequest{
-		Deployment: &types.DeploymentSpec{
-			Repository: &types.GithubRepository{
-				Name:  name,
-				Owner: owner,
-			},
-			DeploymentID: deployment.GetID(),
-		},
-		PayloadSpec: payload,
-		DeliveryID:  deliveryID,
-		Cluster:     cluster,
-		Time:        types.TimeAsTimestamp(now),
-		Deadline:    now.Add(ttl).Unix(),
 	}, nil
 }
--- pkg/hookd/api/v1/deploy/deploymentrequest_test.go
+++ pkg/hookd/api/v1/deploy/deploymentrequest_test.go
@@ -3,7 +3,6 @@ package api_v1_deploy_test
 import (
 	"testing"
 
-	gh "github.com/google/go-github/v27/github"
 	server "github.com/navikt/deployment/pkg/hookd/api/v1/deploy"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,27 +14,6 @@ const (
 	environment = "some environment"
 	payload     = `{"team":"my team","kubernetes":{"resources":[{"apiVersion":"v1","kind":"ConfigMap","metadata":{"labels":{"team":"my team"},"name":"foobar","namespace":"default"}}]}}`
 )
-
-func TestDeploymentRequestFromEvent(t *testing.T) {
-	t.Run("well-formed deployment event returns a deployment request", func(t *testing.T) {
-		ev := &gh.DeploymentEvent{
-			Repo: &gh.Repository{
-				FullName: gh.String(repoName),
-			},
-			Deployment: &gh.Deployment{
-				Environment: gh.String(environment),
-				Payload:     []byte(payload),
-			},
-		}
-		req, err := server.DeploymentRequestFromEvent(ev, deliveryID)
-		assert.NoError(t, err)
-		assert.NotNil(t, req)
-		assert.Equal(t, deliveryID, req.GetDeliveryID())
-		assert.Equal(t, environment, req.GetCluster())
-		assert.Equal(t, repoName, req.GetDeployment().GetRepository().FullName())
-		assert.Equal(t, team, req.GetPayloadSpec().GetTeam())
-	})
-}
 
 func TestDeploymentRequest(t *testing.T) {
 	deploymentRequest := &server.DeploymentRequest{}
