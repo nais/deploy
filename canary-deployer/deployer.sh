ce07faef9e45bf1a8bb352db1163f16599a9849c
Kim Tore Jensen <kimtjen@gmail.com>
2021-03-18 10:11:03 +0100

    canary application: remove ingress

--- canary-deployer/deployer.sh
+++ canary-deployer/deployer.sh
@@ -6,7 +6,6 @@ for CLUSTER in $CLUSTERS; do
   export CLUSTER
   echo "---" > $VARS
   echo "now: $(date +%s)000000000" >> $VARS
-  echo "fqdn: nais-deploy-canary.$CLUSTER.nais.io" >> $VARS
   echo "Deploying to $CLUSTER..."
   /app/deploy --wait=false
 done
