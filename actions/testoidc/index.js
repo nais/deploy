9fe67259dfe4826178ca509199b4e303dccc7648
J-K. Solbakken <jksolbakken@gmail.com>
2023-06-19 09:46:25 +0200

    decode without using a library, avoid having to push node_modules

--- actions/testoidc/index.js
+++ actions/testoidc/index.js
@@ -1,10 +1,13 @@
 const core = require('@actions/core')
-const jose = require('jose')
+
+function decode(token) {
+	return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+}
 
 try {
 	const aud = core.getInput('aud')
 	core.getIDToken(aud).then(token => {
-		const claims = jose.decodeJwt(token)
+		const claims = decode(token)
 		core.setOutput('result', claims)
 	})
 } catch (err) {
