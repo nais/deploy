b86ba67d7af03793de0a3665dc81c95158cceb33
J-K. Solbakken <jksolbakken@gmail.com>
2023-06-19 09:02:09 +0200

    should be a complete runnable action

--- actions/testoidc/index.js
+++ actions/testoidc/index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core')
 try {
 	const aud = core.getInput('aud')
 	console.log(`hello future audience ${aud}`)
+	core.setOutput("result", "resulty things")
 } catch (err) {
 	core.setFailed(err.message)
 }
