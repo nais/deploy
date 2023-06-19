dfd4a1bfe5c0c3e6eba3945891f5f71b801e3e25
J-K. Solbakken <jksolbakken@gmail.com>
2023-06-19 09:13:53 +0200

    request token with custom aud

--- actions/testoidc/index.js
+++ actions/testoidc/index.js
@@ -3,7 +3,10 @@ const core = require('@actions/core')
 try {
 	const aud = core.getInput('aud')
 	console.log(`hello future audience ${aud}`)
-	core.setOutput("result", "resulty things")
+	core.getIDToken(aud).then(token => {
+		console.log(`got: ${token}`)
+		core.setOutput("token", token)
+	})
 } catch (err) {
 	core.setFailed(err.message)
 }
