8c05c5c3c239e3a8418d3cede7cf534dab62ddb8
J-K. Solbakken <jksolbakken@gmail.com>
2023-06-19 09:40:15 +0200

    decode token to view claims

--- actions/testoidc/index.js
+++ actions/testoidc/index.js
@@ -3,7 +3,6 @@ const jose = require('jose')
 
 try {
 	const aud = core.getInput('aud')
-	console.log(`hello future audience ${aud}`)
 	core.getIDToken(aud).then(token => {
 		const claims = jose.decodeJwt(token)
 		core.setOutput('result', claims)
