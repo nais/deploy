dc7227f113a054fd8ad763a88df469a68fb883e7
J-K. Solbakken <jksolbakken@gmail.com>
2023-06-19 09:21:27 +0200

    encode result so we can see it

--- actions/testoidc/index.js
+++ actions/testoidc/index.js
@@ -4,8 +4,9 @@ try {
 	const aud = core.getInput('aud')
 	console.log(`hello future audience ${aud}`)
 	core.getIDToken(aud).then(token => {
-		console.log(`got: ${token}`)
-		core.setOutput("token", token)
+		const buf = new Buffer(token)
+		const tokenb64 = buf.toString('base64')
+		core.setOutput('result', tokenb64)
 	})
 } catch (err) {
 	core.setFailed(err.message)
