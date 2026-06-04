84ccafb08ec951f0d50cee6c65acbd61d97268f5
Kyrre Havik <kyrre.havik@nav.no>
2026-06-04 14:28:48 +0200

    upkeep: tools-fil

---
+++ tools.go
@@ -0,0 +1,12 @@
+//go:build tools
+
+package tools
+
+import (
+	_ "github.com/vektra/mockery/v2"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+	_ "mvdan.cc/gofumpt"
+)
